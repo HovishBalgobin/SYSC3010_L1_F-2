71b584931f566b60a3bd7e40465b3b9e0dbf8ad1
HovishBalgobin <hovishbalgobin@cmail.carleton.ca>
2020-10-09 22:51:09 +0400

    Lab 4 exam

--- Lab-4/lab4.py
+++ Lab-4/lab4.py
@@ -1,19 +1,65 @@
-from urllib.parse import urlencode 
-import time
-import http.client as http
-from urllib.request import urlopen
+from sense_hat import SenseHat
+from time import sleep
+counter =0
 
-key = "7481QW0APO2BO2BU"
-Project_Group= "L1-f-2"
-Member_Identifier="a"
-cmail_address="hovishbalgobin@cmail.carleton.ca"
 
-URL='http://api.thingspeak.com/update?api_key='
-HEADER='&field1={}&field2{}&field3{}'.format(Project_Group,Member_Identifier,cmail_address)
-new_url=URL+key+HEADER
-print(new_url)
-update= urlopen(new_url)
-print(update)
+sense = SenseHat()
+## creating a variable to make use of the SenseHat in-built functions
+sense.clear()
+## clearing the display
+
+purple = (148,0,211)
+##creating purple using the RGB values
+
+def show_H():
+  sense.show_letter("H",back_colour= purple)
+  #time.sleep(.5)
+  
+##function to display H whenever it is called with a purple background.
+  
+def show_B():
+  sense.show_letter("B", back_colour = purple)
+  #time.sleep(.5)
+  
+##function to display B whenever it is called with a purple background.
+
+def repeat(flag):
+    if (flag==True):
+        show_H()
+        
+       
+    elif(flag==False):
+        show_B()
+        #flag = True
+        
+    flag=not(flag)
+    return flag
+
+## function used to display the letters and toggle the flag.
+
+    
+selection = False    #initialising selection and Flag    
+Flag= True;
+while True: 
+  
+  events = sense.stick.get_events()
+  for event in events:
+    # Skip releases
+    if event.action != "released":        
+         Flag = repeat(Flag)
+         if (sense.stick.get_events()==UP):
+             counter++
+         if  (sense.stick.get_events()==DOWN):
+            counter--
+        
+         selection = True
+         
+sense.clear()
+
+
+
+
+
 
 
 
