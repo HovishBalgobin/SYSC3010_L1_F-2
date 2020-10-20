2b9e5217360a07b063b9e163072df622c7ec7fa4
Jack Harold <jackharold019@gmail.com>
2020-10-20 17:34:19 -0400

    Added team member B contribution

--- Lab-5/AB.py
+++ Lab-5/AB.py
@@ -1,7 +1,8 @@
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
 from A import * ##Written by team member A, Hovish Balgobin
+from B import * # Contributions from team member B, Jack Harold
 def function_AB():
     print ("AB")
     function_A()
- 
+    function_B()
---
+++ Lab-5/B.py
@@ -0,0 +1,4 @@
+# Created by member B (Jack Harold)
+
+def function_B():
+  print("B")
