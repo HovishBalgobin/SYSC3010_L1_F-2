f15389f2a854bbc85d1e6f253b3d72851e8258fc
Mehdi Khan <mehdi.khan@carleton.ca>
2020-12-04 13:54:45 -0500

    small edits in email file

--- Dashboard Module/email_notif.py
+++ Dashboard Module/email_notif.py
@@ -6,11 +6,13 @@ def email_alert(reason_str, r_email):
     server.login("babymonitor3010@gmail.com","termproject")
 
     recipient = r_email
-    msg = "From: babymonitor3010@gmail.com\nTo:" + recipient +"\nSubject: " + reason_str
-    msg = msg + "\n\nThe baby monitering system has read data out of set bounds. \nPlease check on the baby\n"
+    msg = "From: babymonitor3010@gmail.com\nTo:" + recipient +"\nSubject: Smart Crib Alert"
+    msg = msg + "\n\nThe baby monitering system has read data out of set bounds. \n"
+    msg = msg + "This notification was caused by:\n\n" + reason_str
+    msg = msg + "\n\nPlease check on the baby. \n"
 
     try:
-        server.sendmail('test_email@zoho.com',recipient,msg)
+        server.sendmail('babymonitor3010@gmail.com',recipient,msg)
     except:
         return server
 
